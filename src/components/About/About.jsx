8eddcb5eaaf298c64c1d7e21629be7085c5a7e33
AdwinTS <adwintsunil8@gmail.com>
2024-10-06 21:22:11 +0530

    Updates are done as of now

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,8 +19,7 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                I'm a frontend developer with experience in using React,html ,css,javascriptand typescript
               </p>
             </div>
           </li>
@@ -29,8 +28,7 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>Backend Developer</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                I have experience in using node.js, django and flask 
               </p>
             </div>
           </li>
@@ -39,8 +37,7 @@ export const About = () => {
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                I have started to design enjoyable superby designs
               </p>
             </div>
           </li>
