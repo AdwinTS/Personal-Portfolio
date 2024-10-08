0724333024196c1c5a4603fca9ce6752fd37c7f8
Adwin T Sunil <145273482+AdwinTS@users.noreply.github.com>
2024-10-08 23:33:10 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/adwintsunil">linkedin.com/adwintsunil</a>
+          <a href="https://www.linkedin.com/in/adwintsunil">linkedin.com/adwintsunil</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
